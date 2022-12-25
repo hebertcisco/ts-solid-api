0fecebb57790142171a9d082f67a3526d3e5a8c7
hebertcisco <hebertcisco@outlook.com>
2022-12-25 00:51:08 +0000

    refactor: added index route

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,17 @@
 import { Router } from "express";
+import type { Request, Response } from "express";
+
 import { createUserController } from "./useCases/CreateUser";
 
 const router = Router();
 
-router.post("/users", (request, response) => {
+router.get("/", (request: Request, response: Response) => {
+  return response.json({
+    message: "Welcome!"
+  });
+});
+
+router.post("/users", (request: Request, response: Response) => {
   return createUserController.handle(request, response);
 });
 
