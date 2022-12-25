98a499ba37f4409d8b3715fd6ca19840e4795ce5
hebertcisco <hebertcisco@outlook.com>
2022-12-25 00:47:41 +0000

    refactor: adding Definite Assignment setting on user

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -1,18 +1,18 @@
-import { uuid } from "uuidv4";
+import crypto from 'node:crypto';
 
 export class User {
     
-  public readonly id: string;
+  public readonly id!: string;
 
-  public name: string;
-  public email: string;
-  public password: string;
+  public name!: string;
+  public email!: string;
+  public password!: string;
 
   constructor(props: Omit<User, "id">, id?: string) {
     Object.assign(this, props);
 
     if (!id) {
-      this.id = uuid();
+      this.id = crypto.randomUUID();
     }
   }
 }
